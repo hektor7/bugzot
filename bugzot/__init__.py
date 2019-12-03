48138ec793f6975931b2c8cee9f88fc3d2c6d569
Héctor Vizcaíno <hektor7@gmail.com>
2019-12-03 18:10:22 +0100

    Export models and generate them.

--- bugzot/__init__.py
+++ bugzot/__init__.py
@@ -3,3 +3,6 @@ File: __init__.py
 Description: Bugzot application entrypoint file.
 '''
 from .application import app, bcrypt, db
+from bugzot.models import ActivationKey, Category, Component, Product, Role, User, Version
+
+db.create_all()
---
+++ bugzot/models/__init__.py
@@ -0,0 +1,7 @@
+from .users import User
+from .users import Role
+from .users import ActivationKey
+from .products import Category
+from .products import Component
+from .products import Product
+from .products import Version
