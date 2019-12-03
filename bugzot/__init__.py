1f1ef37b9815835d3374ea01c0b7a0305dc70c8f
Héctor Vizcaíno <hektor7@gmail.com>
2019-12-03 18:32:34 +0100

    Add index view.

--- bugzot/__init__.py
+++ bugzot/__init__.py
@@ -4,5 +4,9 @@ Description: Bugzot application entrypoint file.
 '''
 from .application import app, bcrypt, db
 from bugzot.models import ActivationKey, Category, Component, Product, Role, User, Version
+from bugzot.views import IndexView
+
+app.add_url_rule('/', view_func=IndexView.as_view('index_view'))
+
 
 db.create_all()
